1f75149d4ce650d20b19b97f92aca3424ed83837
linder <achielinder@gmail.com>
2023-11-24 21:31:27 +0300

    completed assignments

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    return even_numbers
+    
+num_list = [1,2,3,4,5,6,7,8,9,10]
+result = return_evens(num_list)
+print (result)
 
 def make_exclamation(sentence_list):
-    pass
+    sentences_with_exclamation = [sentence + '!' for sentence in sentence_list]
+    return sentences_with_exclamation
+
+sentence_list =[ "Hello", "I'm doing great", "Python is fun"]
+result= make_exclamation(sentence_list)
+print(result)
